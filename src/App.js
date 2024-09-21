b5268a28b0850d1d2085611e6dfaf221bb069317
vinayak <vinayak.vallakati@gmail.com>
2024-09-21 13:04:28 +0530

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
